60473c1b9490a0d10b092fdc3478c1aef64fee7a
Muneeb Ansari <muneebansari658@gmail.com>
2020-09-07 23:18:38 -0400

    add as data access object to interface tweet retrieval and posts

---
+++ 3ds_twitter/source/twitter_data_service.cpp
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <cstdarg>
+#include <cstdio>
+#include <cstring>
+#include <sys/types.h>
+#include <curl/curl.h>
+#include "http_client.h"
+#include "twitter_data_service.h"
+
+TwitterDataService::TwitterDataService(HttpClient httpClient)
+{
+    this->httpClient = httpClient;
+}
+
+Response TwitterDataService::getUserTweets(char *id)
+{
+    CURLcode res;
+
+    struct curl_slist *headers = NULL;
+    this->httpClient.setRequestHeaders(&headers);
+
+    Response responseChunk;
+    responseChunk.data = NULL;
+    responseChunk.size = 0;
+
+    if ((res = this->httpClient.GET(
+             (char const *)"http://localhost:3001/hello",
+             headers,
+             this->responseCallback,
+             &responseChunk)) != CURLE_OK)
+    {
+        fprintf(stderr, "getUserTweets -> curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+    }
+    return responseChunk;
+}
+
+/*
+    Reference: https://curl.haxx.se/libcurl/c/CURLOPT_WRITEFUNCTION.html
+    This callback function gets called by libcurl as soon as there is data 
+    received that needs to be saved. For most transfers, this callback gets 
+    called many times and each invoke delivers another chunk of data.
+*/
+size_t TwitterDataService::
+    responseCallback(void *contents,
+                     size_t size,
+                     size_t nmemb,
+                     void *responseChunk)
+{
+    size_t realsize = size * nmemb;
+    Response *resp = (Response *)responseChunk;
+
+    char *ptr = (char *)realloc(resp->data, resp->size + realsize + 1);
+    if (ptr == NULL)
+    {
+        /* out of memory! */
+        printf("not enough memory (realloc returned NULL)\n");
+        return 0;
+    }
+
+    resp->data = ptr;
+    memcpy(&(resp->data[resp->size]), contents, realsize);
+    resp->size += realsize;
+    resp->data[resp->size] = 0;
+    return realsize;
+}
---
+++ 3ds_twitter/source/twitter_data_service.h
@@ -0,0 +1,26 @@
+#ifndef TWITTER_DATA_SERVICE_H
+#define TWITTER_DATA_SERVICE_H
+
+#include <stdlib.h>
+#include <cstdarg>
+#include <cstdio>
+#include <cstring>
+#include <sys/types.h>
+#include <curl/curl.h>
+#include "http_client.h"
+
+class TwitterDataService
+{
+
+private:
+    HttpClient httpClient;
+
+public:
+    TwitterDataService(HttpClient httpClient);
+    Response getUserTweets(char *id);
+    static size_t responseCallback(void *contents,
+                                   size_t size,
+                                   size_t nmemb,
+                                   void *responseChunk);
+};
+#endif
