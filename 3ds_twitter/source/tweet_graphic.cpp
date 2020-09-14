5eb8d182ff1b6f5e2742c0c4be8ae212d387f56c
Muneeb Ansari <muneebansari658@gmail.com>
2020-09-14 16:04:03 -0400

    draw fav icon graphic on tweet

--- 3ds_twitter/source/tweet_graphic.cpp
+++ 3ds_twitter/source/tweet_graphic.cpp
@@ -7,6 +7,7 @@
 #include <cstdio>
 #include <stdlib.h>
 #include "text_graphic.h"
+#include "image_graphic.h"
 #include "tweet_graphic.h"
 
 const u32 TweetGraphic::bgClr = C2D_Color32(0xE3, 0xF1, 0xFC, 0xFF);
@@ -16,55 +17,46 @@ TweetGraphic::TweetGraphic(Tweet *tweet, float xStart, float yStart)
     this->tweet = tweet;
     this->xStart = xStart;
     this->yStart = yStart;
+    this->initBounds();
 }
 
-float TweetGraphic::getHeight()
+float &TweetGraphic::getHeight()
 {
     return this->height;
 }
 
-float TweetGraphic::getWidth()
+float &TweetGraphic::getWidth()
 {
     return this->width;
 }
 
-Tweet *TweetGraphic::getTweet()
+Tweet &TweetGraphic::getTweet()
 {
-    return this->tweet;
+    return *this->tweet;
 }
 
 void TweetGraphic::draw()
 {
-
-    TextGraphic tweetTextGraphic =
-        TextGraphic((this->tweet)->text,
-                    300,
-                    C2D_AlignLeft | C2D_WordWrap,
-                    this->xStart + 5,
-                    this->yStart + 5,
-                    0,
-                    0.5f,
-                    0.5f);
-
-    // Set tweet container height and width to fit tweet content
-    tweetTextGraphic.getDimensions(&this->width, &this->height);
-    this->height = (this->height * (tweetTextGraphic.getC2DText().width / 1569)) + 30;
-    this->width = GSP_SCREEN_HEIGHT_TOP - 40;
-
     // Initialize fav count text
     char favCountBuf[7];
     sprintf(favCountBuf, "%d", (this->tweet)->favCount);
     const char *favCount = favCountBuf;
 
-    TextGraphic favTextGraphic =
+    TextGraphic favCountTextGraphic =
         TextGraphic(favCount,
                     7,
                     C2D_AlignLeft,
-                    this->xStart + this->width - 30,
+                    this->xStart + this->width - 40,
                     this->yStart + this->height - 15,
                     0,
                     0.5f,
                     0.5f);
+
+    //Initialize fav graphic
+    C2D_Image heart;
+    C3D_Tex heartTex;
+    ImageGraphic favHeartGraphic = ImageGraphic(&heart);
+    favHeartGraphic.applyTexture(&heartTex, "romfs:/gfx/heart.t3x", 16, 16);
 
     // Draw tweet container
     C2D_DrawRectSolid(this->xStart,
@@ -75,8 +67,31 @@ void TweetGraphic::draw()
                       this->bgClr);
 
     // Draw text within tweet container and add word wrap with a padding of 5.
-    tweetTextGraphic.draw(this->width - 5);
+    float wordWrap = this->width - 5;
+    this->tweetTextGraphic.draw(wordWrap);
 
-    // Draw fav text
-    favTextGraphic.draw();
+    // Draw fav heart graphic
+    C2D_DrawImageAt(heart, this->xStart + this->width - 20, this->yStart + this->height - 20, 0);
+
+    // Draw fav count text
+    favCountTextGraphic.draw();
+}
+
+void TweetGraphic::initBounds()
+{
+    // Initialize tweet text
+    this->tweetTextGraphic =
+        TextGraphic((this->tweet)->text,
+                    300,
+                    C2D_AlignLeft | C2D_WordWrap,
+                    this->xStart + 5,
+                    this->yStart + 5,
+                    0,
+                    0.5f,
+                    0.5f);
+
+    //Set tweet container height and width to fit tweet content
+    this->tweetTextGraphic.getDimensions(&this->width, &this->height);
+    this->height = (this->height * (this->tweetTextGraphic.getC2DText().width / 1569)) + 30;
+    this->width = GSP_SCREEN_HEIGHT_TOP - 40;
 }
--- 3ds_twitter/source/tweet_graphic.h
+++ 3ds_twitter/source/tweet_graphic.h
@@ -10,24 +10,29 @@
 #include <cstdio>
 #include <stdlib.h>
 #include <string>
+#include "image_graphic.h"
 #include "text_graphic.h"
 #include "models.h"
 
 class TweetGraphic
 {
 private:
+    Tweet *tweet;
+    TextGraphic tweetTextGraphic;
     float height;
     float width;
     float xStart;
     float yStart;
     static const u32 bgClr;
-    Tweet *tweet;
+
+    void initBounds();
 
 public:
     TweetGraphic(Tweet *tweet, float xStart, float yStart);
-    float getHeight();
-    float getWidth();
-    Tweet *getTweet();
+
+    float &getHeight();
+    float &getWidth();
+    Tweet &getTweet();
     void draw();
 };
 
